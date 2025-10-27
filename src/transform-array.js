2cc8dac774e12134154771ecd11f5f0fbfc45416
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2025-10-27 20:26:46 +0300

    feat: implement 'transform-array' solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const {NotImplementedError} = require('../lib');
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,9 +13,44 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
+  if (!arr.length) return arr;
+
+  const result = [];
+  let lastItem = null;
+  let isDeleted = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next': {
+        i++;
+        isDeleted = true;
+        break;
+      }
+      case '--discard-prev': {
+        if (lastItem !== '--discard-next') result.pop();
+        break;
+      }
+      case '--double-next':
+        if (arr[i + 1] !== undefined) result.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        if (lastItem !== '--discard-next' && i !== 0) result.push(result.at(-1));
+        break;
+      default: {
+        result.push(arr[i]);
+        break;
+      }
+    }
+    lastItem = isDeleted ? '--discard-next' : arr[i];
+    isDeleted = false;
+  }
+
+  return result;
 }
 
 module.exports = {
