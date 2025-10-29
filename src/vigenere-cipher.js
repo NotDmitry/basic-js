98645a77f26b0377bec22305df1f53e165504074
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2025-10-29 16:28:05 +0300

    feat: implement 'vigenere-cipher' solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const {NotImplementedError} = require('../lib');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,14 +20,57 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+
+  constructor(isDirect = true) {
+    this.isDirectMachine = isDirect;
+    this.vocabulary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  getResult(message, key, operation) {
+    const messageUpper = message.toUpperCase();
+    const keyUpper = key.toUpperCase();
+    const length = this.vocabulary.length;
+    let keyIndex = 0;
+    let result = '';
+
+    for (let i = 0; i < messageUpper.length; i++) {
+      const messageCharCode = this.vocabulary.indexOf(messageUpper[i]);
+      if (messageCharCode !== -1) {
+        const keyCharCode = this.vocabulary.indexOf(keyUpper[keyIndex]);
+        let charCode;
+        switch (operation) {
+          case 'encrypt': charCode = messageCharCode + keyCharCode; break;
+          case 'decrypt': charCode = messageCharCode - keyCharCode; break;
+          default: throw new Error('Unknown operation call');
+        }
+        let index = (charCode % length) + length;
+        result += this.vocabulary[index % length];
+        keyIndex = (keyIndex + 1) % keyUpper.length;
+      } else {
+        result += messageUpper[i];
+      }
+    }
+
+    if (this.isDirectMachine) return result;
+    return result.split('')
+      .reverse()
+      .join('');
+  }
+
+  encrypt(message, key) {
+    if (arguments.length < 2 || message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    return this.getResult(message, key, 'encrypt');
+  }
+
+  decrypt(message, key) {
+    if (arguments.length < 2 || message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    return this.getResult(message, key, 'decrypt');
   }
 }
 
